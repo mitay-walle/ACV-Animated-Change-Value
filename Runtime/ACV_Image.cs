a176c2964c7326d34bfcc6500a248829536d864a
mitay-walle <mitay-walle91@mail.ru>
2022-04-14 10:30:12 +0300

    Samples/ACV_SlicedFilledImage

---
+++ Runtime/ACV_Image.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.UI;
+
+namespace Plugins.mitaywalle.ACV.Runtime
+{
+	public class ACV_Image : ACV_Component<Image>
+	{
+		public override void SetValue()
+		{
+			_target.fillAmount = (_animatedValue - _minValue) * 1f / (_maxValue - _minValue);
+
+			base.SetValue();
+		}
+
+		public override void DisableTarget()
+		{
+			_target.gameObject.SetActive(false);
+		}
+	}
+}
---
+++ Samples/ACV_SlicedFilledImage/Scripts/ACV_SlicedFilledImage.cs
@@ -0,0 +1,17 @@
+﻿namespace Plugins.mitaywalle.ACV.Runtime
+{
+	public class ACV_SlicedFilledImage : ACV_Component<SlicedFilledImage>
+	{
+		public override void SetValue()
+		{
+			_target.fillAmount = (_animatedValue - _minValue) * 1f / (_maxValue - _minValue);
+
+			base.SetValue();
+		}
+
+		public override void DisableTarget()
+		{
+			_target.gameObject.SetActive(false);
+		}
+	}
+}
